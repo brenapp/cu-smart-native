4701701ca026cbdf689183e6f399912c9b20829f
billzyj <yongjia@clemson.edu>
2021-05-31 23:34:18 -0400

    Initial commit

---
+++ packages/frontend/screens/RoomSelectionScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Button,
+  useColorScheme,
+  View,
+} from "react-native";
+
+const RoomSelectionScreen = props => {
+  const clickButtionHandler = () => {
+    props.navigation.navigate('UserTab');
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button title="Press me" onPress={clickButtionHandler}/>
+    </View>
+  );
+};
+export default RoomSelectionScreen;
