195d9e2efd7891a4c6ad6aa9637dfa9e1a9b94b9
billzyj <yongjia@clemson.edu>
2021-06-01 00:18:55 -0400

    Second Commit

---
+++ packages/frontend/components/HeatMapChart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform
+} from 'react-native';
+
+const HeatMapChart = props =>{
+  return (
+
+  );
+}
+
+export default HeatMapChart;
