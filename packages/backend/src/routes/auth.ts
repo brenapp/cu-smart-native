d3a6e18eb81a671e67cc890b143de7e95c2a5db5
Brendan McGuire <bmmcgui@clemson.edu>
2021-06-30 05:20:39 -0400

    Add handler for callback

--- packages/backend/src/routes/auth.ts
+++ packages/backend/src/routes/auth.ts
@@ -5,15 +5,29 @@
 import app from "../main"
 import passport from "passport"
 import { Strategy as SAMLStrategy } from "passport-saml"
+import * as bodyParser from "body-parser"
 
 passport.use("sso", new SAMLStrategy({
-    path: "/login/callback",
+    path: "/auth/callback",
     entryPoint: "https://idp.clemson.edu/idp/profile/SAML2/Redirect/SSO",
     issuer: "CEVAC",
     cert: "cert"
 }, (request, profile, done) => {
-
+    console.log("AUTH", profile);
+    done(null);
 }))
 
 
 app.get("/auth", passport.authenticate("sso"))
+
+
+app.post(
+    "/auth/callback",
+    bodyParser.urlencoded({ extended: true }),
+    passport.authenticate("sso", { failureRedirect: "/", failureFlash: true }),
+    function (req, res) {
+        console.log(req.user, req.isAuthenticated())
+
+      res.redirect("/");
+    }
+  );
