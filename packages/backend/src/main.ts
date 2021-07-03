0197ab88483cc85c0a27822196d7ad690356e02a
Brendan McGuire <bmmcgui@clemson.edu>
2021-07-03 13:36:49 -0400

    Update live message

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,9 +3,8 @@ import express from "express"
 
 const app = express();
 app.listen(8080, () => {
-    console.log("cu-smart-backend live on port 8080")
+    console.log("live on port 8080")
 });
-
 
 export default app;
 import "./routes"
