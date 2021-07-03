1c0966ccd9c4fc9f5b91cef8d4c5488747d772f3
Brendan McGuire <bmmcgui@clemson.edu>
2021-07-03 13:37:26 -0400

    Add winston logging

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,1 +1,41 @@
-import "./auth"
+import auth from "./auth"
+import feedback from "./feedback"
+
+import app from "../main"
+import winston from "winston"
+import expressWinston from "express-winston"
+
+
+// Setup logging 
+app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.cli()
+    )
+  }));
+
+// Routers
+app.use(auth);
+app.use(feedback);
+
+// Default handlers
+app.use((req, res) => {
+    res.status(404).json({
+        "status": "error",
+        "error_mesage": "Could not find route specified"
+    });
+});
+
+// Error handling 
+app.use(expressWinston.errorLogger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.cli()
+    )
+  }));
