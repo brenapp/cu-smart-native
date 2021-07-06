145f30b808aa4943aeedb80ebd0e366a1254bf56
Brendan McGuire <brendan@bren.app>
2021-07-06 14:40:06 -0400

    Add developer authentication method based on prebaked credentials

--- packages/backend/src/routes/auth.ts
+++ packages/backend/src/routes/auth.ts
@@ -1,11 +1,15 @@
 /**
- * Handles Single Sign through Clemson's System using SAML2 over Shibboleth, using passport and passport-saml. 
+ * Handles Single Sign through Clemson's System using SAML2 over Shibboleth, using passport and
+ * passport-saml. Additionally provides a development login system using simple usernames and
+ * passwords. This system is temporary until we can get the authentication system working!
  */
 
 import passport from "passport"
 import { Strategy as SAMLStrategy } from "passport-saml"
+import { Strategy as LocalStrategy } from "passport-local"
 import * as bodyParser from "body-parser"
 import { Router } from "express"
+import { devCredentials } from "../config.json" 
 
 passport.use("sso", new SAMLStrategy({
   path: "/auth/callback",
@@ -18,9 +22,29 @@ passport.use("sso", new SAMLStrategy({
 }))
 
 
+
+passport.use("local", new LocalStrategy((username, password, done) => {
+
+  const usernames = Object.keys(devCredentials);
+
+  if (!usernames.includes(username)) {
+    return done(null, false, { "message": "Incorrect username" })
+  }
+
+  // Cast: we are ensuring that username is a key in the above section
+  if (password != (devCredentials as Record<string, string>)[username]) {
+    return done(null, false, { "message": "Incorrect password" })
+  };
+
+  return done(null, username);
+
+}));
+
+
 const router = Router();
 
-router.get("/auth", passport.authenticate("sso"))
+router.get("/auth", passport.authenticate("sso"));
+router.get("/auth/dev", passport.authenticate("local"))
 
 router.post(
   "/auth/callback",
