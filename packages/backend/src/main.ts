e47924110e47642e62feba62cde7058f972dbe71
Brendan McGuire <brendan@bren.app>
2021-07-09 22:21:38 -0400

    Switch to port 3000

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,8 +2,8 @@ import express from "express"
 
 
 const app = express();
-app.listen(8080, () => {
-    console.log("live on port 8080")
+app.listen(3000, () => {
+    console.log("live on port 3000")
 });
 
 export default app;
