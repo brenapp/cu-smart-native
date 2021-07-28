4db48684d58de9a40f4f1a85a44d1224046e99e5
Brendan McGuire <bmmcgui@clemson.edu>
2021-07-28 19:42:58 -0400

    Initial user database setup

---
+++ packages/backend/src/database/index.ts
@@ -0,0 +1,85 @@
+/**
+ * Manages database access. For right now, this is just an sqlite database in a file, but this may
+ * transition to a more """real""" database in the future.
+ * 
+ * Advantages of the current setup:
+ *  - Very simple
+ *  - Very fast
+ *  - Easy to export data to help develop the model
+ *
+ * Disadvantages of the current setup:
+ *  - Will not have networked access to data
+ *  - Little bit harder to administrate
+ * 
+ **/
+
+import sqlite3 from "sqlite3";
+import * as sqlite from "sqlite";
+import { sqlite as config } from "../config.json";
+import { logger } from "../main";
+
+export async function database() {
+    return sqlite.open({
+        filename: config.path,
+        driver: sqlite3.cached.Database,
+    });
+};
+
+/**
+ * Ensures the database is setup correctly.
+ **/
+export async function ensureSchema() {
+    logger.info("Ensuring database compatibility...");
+    const db = await database();
+    
+    // Users
+    logger.info("Ensuring database compatibility: users...");
+    await db.run(
+        `CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY, 
+            username TEXT, 
+            admin INTEGER, 
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP);
+    `);
+
+    // User favorites
+    logger.info("Ensuring database compatibility: favorites...");
+    await db.run(
+        `CREATE TABLE IF NOT EXISTS favorites (
+            user_id INTEGER, 
+            place_id INTEGER, 
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP);
+    `);
+    
+    // Feedback submissions
+    logger.info("Ensuring database compatibility: feedback...");
+    await db.run(
+        `CREATE TABLE IF NOT EXISTS feedback (
+            user_id INTEGER, 
+            place_id INTEGER,
+            overall_satisfaction INTEGER,
+            sensations_temperature INTEGER,
+            sensations_air_quality INTEGER,
+            preferences_temperature INTEGER,
+            preferences_light INTEGER,
+            preferences_sound INTEGER, 
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP);
+    `);
+
+    // Tokens
+    logger.info("Ensuring database compatibility: tokens...");
+    await db.run(
+        `CREATE TABLE IF NOT EXISTS tokens (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            token TEXT,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP);
+    `);
+
+    logger.info("Ensuring database compatibility: COMPLETE!");
+
+    const users = await db.get("SELECT * FROM USERS");
+    logger.info(JSON.stringify(users));
+    
+
+};
---
+++ packages/backend/src/database/user.ts
@@ -0,0 +1,235 @@
+/**
+ * Models a user in the database.
+ **/
+
+import { database } from "./index"
+import { FivePointScale, UserFeedback } from "../routes/feedback";
+
+export interface UserData {
+    id: number;
+    username: string;
+    admin: number;
+    created: Date;
+};
+
+// All the different ways we can identify a single user.
+interface UserIdentifier {
+    username: string;
+    id: number;
+    token: string;
+};
+
+export default class User {
+
+    /**
+     * Ensures a specific user exists, given some information about them. Does not ensure that the
+     * given information is accurate for given users, instead the passed data is used for new users.
+     * 
+     * @param data Default user data to use if the user does not exist.
+     * @returns The created/existing user.
+     **/
+    static async ensure(data: Omit<Omit<UserData, "created">, "id">): Promise<User> {
+        const db = await database();
+        let user = await User.by("username", data.username);
+
+        if (!user) {
+            const result = await db.run("INSERT INTO users (username, admin) VALUES (?, ?)", data.username, data.admin);
+            user = new User({
+                id: result.lastID as number,
+                created: new Date(),
+                ...data
+            });
+        };
+
+        return user;
+    }
+
+
+    /**
+     * Gets a specific user based on an individual identifier, 
+     * 
+     * @param characteristic The characteristic to use
+     * @param value The value to search for
+     * 
+     * @returns The user if it exists, otherwise null
+     **/
+    static async by<T extends keyof UserIdentifier>(characteristic: T, value: UserIdentifier[T]): Promise<User | null> {
+        const db = await database();
+
+        let data: UserData | undefined = undefined;
+        switch (characteristic) {
+            case "id": {
+                data = await db.get<UserData>("SELECT * FROM users WHERE id = ?", value.toString());
+                break;
+            };
+
+            case "username": {
+                data = await db.get<UserData>("SELECT * FROM users WHERE username = ?", value.toString());
+                break;
+            };
+
+            case "token": {
+
+                const id = await db.get("SELECT user_id FROM tokens WHERE token = ?", value.toString());
+
+                if (id) {
+                    data = await db.get<UserData>("SELECT * FROM users WHERE id = ?", id);
+                }
+                break;
+            };
+        };
+
+        if (data) {
+
+            // Turn created into a date
+            data.created = new Date(data.created);
+
+            return new User(data);
+        } else {
+            return null;
+        }
+
+    };
+
+    data: UserData;
+    constructor(data: UserData) {
+        this.data = data;
+    };
+
+    /**
+     * Updates information about this user in the database.
+     * 
+     * @param key The column to update 
+     * @param value The new value for the column
+     * 
+     * @returns The result of the update
+     **/
+    async update<T extends keyof UserData>(key: T, value: UserData[T]) {
+        const db = await database();
+        return db.run(`UPDATE users SET ${key} = ? WHERE id = ?`, key, value.toString(), this.data.id).then(result => {
+            if (result.changes && result.changes > 0) {
+                this.data[key] = value;
+            }
+        });
+    };
+
+    /**
+     * Gets all user tokens associated with this account. 
+     * 
+     * @returns A list of tokens for this user.
+     **/
+    async tokens(): Promise<string[]> {
+        const db = await database();
+        const tokens = await db.all<{ token: string }[]>("SELECT token FROM tokens WHERE user_id = ?", this.data.id);
+
+        return tokens.map(token => token.token);
+    };
+
+    /**
+     * Adds a new token to this user.
+     * 
+     * @param token The token to add
+     * @returns true if the token was added, false otherwise
+     **/
+    async addToken(token: string): Promise<boolean> {
+        const db = await database();
+        const result = await db.run("INSERT INTO tokens (token, user_id) VALUES (?, ?)", token, this.data.id);
+
+        return !!result.changes && result.changes > 0;
+    };
+
+    /**
+     * Deletes the token from the database.
+     * 
+     * @param token The token to delete
+     * @returns true if the token was deleted, false otherwise
+     **/
+    async deleteToken(token: string): Promise<boolean> {
+        const db = await database();
+        const result = await db.run("DELETE FROM tokens WHERE token = ?", token);
+
+        return !!result.changes && result.changes > 0;
+    };
+
+
+    /**
+     * Returns the PointSliceID of all of this user's favorite places.
+     * 
+     * @returns A list of ids of all of this user's favorite places.
+     **/
+    async favorites(): Promise<number[]> {
+        const db = await database();
+        const favorites = await db.all<{ place_id: number }[]>("SELECT place_id FROM favorites WHERE user_id = ?", this.data.id);
+
+        return favorites.map(fav => fav.place_id);
+    };
+
+    /**
+     * Adds a new favorite place to the user.
+     * 
+     * @param favorite The point slice ID of the favorite place to add
+     * @returns true if the favorite place was added, false otherwise
+     **/
+    async addFavorite(favorite: number): Promise<boolean> {
+        const db = await database();
+        const result = await db.run("INSERT INTO favorites (place_id, user_id) VALUES (?, ?)", favorite, this.data.id);
+
+        return !!result.changes && result.changes > 0;
+    };
+
+    /**
+     * Deletes the favorite from the database.
+     * 
+     * @param favorite The favorite to delete
+     * @returns true if the favorite was deleted, false otherwise
+     **/
+    async deleteFavorite(favorite: number): Promise<boolean> {
+        const db = await database();
+        const result = await db.run("DELETE FROM tokens WHERE token = ?", favorite);
+
+        return !!result.changes && result.changes > 0;
+    };
+
+
+    /**
+     * Submit feedback about a particular place.
+     * 
+     * @param
+     **/
+    async addFeedback(feedback: UserFeedback): Promise<boolean> {
+        const db = await database();
+        const result = await db.run(`
+            INSERT INTO feedback 
+                (user_id, 
+                 place_id,
+                 overall_satisfaction,
+                 sensations_temperature,
+                 sensations_air_quality,
+                 preferences_temperature,
+                 preferences_light,
+                 preferences_sound) VALUES (
+                    ?, 
+                    ?, 
+                    ?, 
+                    ?, 
+                    ?, 
+                    ?, 
+                    ?, 
+                    ?)`,
+            this.data.id,
+            feedback.id,
+            feedback.overallSatisfaction,
+            feedback.sensations.temperature,
+            feedback.sensations.airQuality,
+            feedback.preferences.temperature,
+            feedback.preferences.light,
+            feedback.preferences.sound
+        );
+
+        return !!result.changes && result.changes > 0;
+
+    }
+
+
+
+};
