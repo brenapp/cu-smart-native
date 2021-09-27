e5b665d6eb5b5baa067dd66294be5c6b09bd3a44
Brendan McGuire <brendan@bren.app>
2021-09-26 22:54:54 -0400

    Use named volume instead

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ docker push bmmcgui/cu-smart:1.0
 echo "Deploying to FMO14..."
 ssh fmo14 "docker pull bmmcgui/cu-smart:1.0 && 
            docker kill $(docker ps --filter="publish=3000" --format "{{.Names}}") && 
-           docker run -p 3000:3000 -d -v /data --restart always bmmcgui/cu-smart:1.0"
+           docker run -p 3000:3000 -d -v cu-smart-feedback:/data --restart always bmmcgui/cu-smart:1.0"
